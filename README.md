234eb7ddccec3726513ff7df5efed818458d3957
Miki Tebeka <miki.tebeka@gmail.com>
2019-01-13 19:25:55 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Let's Write an Interpreter
+
+## Idea
+
+Book on writing scheme like interpreter in Go.
+
+
+## Resources
+- [lis.py](http://norvig.com/lispy.html)
+- [mal](https://github.com/kanaka/mal)
+- [Bytecode compilers and interpreters](https://bernsteinbear.com/blog/bytecode-interpreters/)
---
+++ lisp.go
@@ -0,0 +1,152 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"strconv"
+	"strings"
+)
+
+type Lexer struct {
+	tokens []string
+	i      int
+}
+
+func NewLexer(code string) *Lexer {
+	code = strings.Replace(code, "(", " ( ", -1)
+	code = strings.Replace(code, ")", " )", -1)
+	tokens := strings.Fields(code)
+	return &Lexer{tokens: tokens, i: -1}
+}
+
+func (l *Lexer) Next() bool {
+	l.i++
+	return l.i < len(l.tokens)
+}
+
+func (l *Lexer) Token() string {
+	return l.tokens[l.i]
+}
+
+func (l *Lexer) Peek() string {
+	if l.i >= len(l.tokens) {
+		return ""
+	}
+	return l.tokens[l.i+1]
+}
+
+type Expr interface {
+	Eval() (Expr, error)
+}
+
+type Number float64
+
+func (n Number) Eval() (Expr, error) {
+	return n, nil
+}
+
+type List []Expr
+
+func (l List) Eval() (Expr, error) {
+	if len(l) != 3 {
+		return nil, fmt.Errorf("wrong number of arguments")
+	}
+
+	op, lhs, rhs := l[0], l[1], l[2]
+
+	lval, err := lhs.Eval()
+	if err != nil {
+		return nil, err
+	}
+
+	nlval, ok := lval.(Number)
+	if !ok {
+		return nil, fmt.Errorf("bad left hand value")
+	}
+
+	rval, err := rhs.Eval()
+	if err != nil {
+		return nil, err
+	}
+
+	nrval, ok := rval.(Number)
+	if !ok {
+		return nil, fmt.Errorf("bad right hand value")
+	}
+
+	sym, ok := op.(Symbol)
+	if !ok {
+		return nil, fmt.Errorf("bad operator - %v", op)
+	}
+
+	switch sym {
+	case "+":
+		return Number(nlval * nrval), nil
+	case "-":
+		return Number(nlval - nrval), nil
+	case "*":
+		return Number(nlval * nrval), nil
+	case "/":
+		return Number(nlval * nrval), nil
+	}
+
+	return nil, fmt.Errorf("unknown operator - %v", sym)
+
+}
+
+type Symbol string
+
+func (s Symbol) Eval() (Expr, error) {
+	return s, nil
+}
+
+func parse(code string) (Expr, error) {
+	lex := NewLexer(code)
+	return readFromTokens(lex)
+}
+
+func readFromTokens(lex *Lexer) (Expr, error) {
+	if !lex.Next() {
+		return nil, io.EOF
+	}
+
+	switch lex.Token() {
+	case "(":
+		var expr List
+		for lex.Peek() != ")" {
+			sub, err := readFromTokens(lex)
+			if err != nil {
+				return nil, err
+			}
+			expr = append(expr, sub)
+		}
+		if !lex.Next() { // Pop )
+			return nil, fmt.Errorf("missing )")
+		}
+		return expr, nil
+	case ")":
+		return nil, fmt.Errorf("unexpected )")
+	default:
+		val, err := strconv.ParseFloat(lex.Token(), 64)
+		if err == nil {
+			return Number(val), nil
+		}
+		return Symbol(lex.Token()), nil
+	}
+}
+
+func main() {
+	code := "(* 3.14 (* 10 10))"
+	//code := "(* 7 2)"
+	prog, err := parse(code)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("prog: %v\n", prog)
+	val, err := prog.Eval()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("val: %v\n", val)
+}
